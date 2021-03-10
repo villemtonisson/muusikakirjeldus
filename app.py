fe25c7c777094e09fba96fb11f8ebeb352128a49
Villem Tõnisson <villemtonisson@gmail.com>
2021-03-10 13:12:21 +0200

    Files are no longer saved locally. Instead they are saved in memory until an answer is produced

--- app.py
+++ app.py
@@ -9,48 +9,46 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#@app.route('/', methods=['POST', 'GET'])
-#def index():
+
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
 #    return render_template('index.html')
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     print(request.url, request.method, request.files)
 
     if request.method == 'POST':
-        print(1)
         # check if the post request has the file part
         if 'filename' not in request.files:
-            # TODO Replace flash
-            flash('No file part')
-            return redirect(request.url)
-        print(2)
+            return render_template('index.html', description="Fail on tühi")
         file = request.files['filename']
         # if user does not select file, browser also
         # submit an empty part without filename
-        print(3)
         if file.filename == '':
-            print(4)
-            # TODO Replace flash
-            flash('No selected file')
-            return redirect(request.url)
+            return render_template('index.html', description="Unustasid faili valida")
         if file and allowed_file(file.filename):
-            print(5)
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-        print(6)
-    return render_template('index.html')
+            # print(file.read().decode("utf8"))
+            # For saving file
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+            # TODO create description
+            description = "Siia tuleb muusikafaili kirjeldus" + "\nÜles laetud fail: " + file.filename
+            return render_template('index.html', description=description)
+
+    return render_template('index.html', description="Lae üles fail kui kirjeldust soovid")
+
+
+# If file is being save locally
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'],
+#                                filename)
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
