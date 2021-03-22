a6a57fe915ac22374bbb20ec305fe7f772285708
Villem Tõnisson <villemtonisson@gmail.com>
2021-03-22 11:42:26 +0200

    File is now saved locally

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, flash
 from werkzeug.utils import secure_filename
+import madmom
 
 UPLOAD_FOLDER = 'uploads'
 # TODO change extensions
@@ -9,10 +10,6 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-# @app.route('/', methods=['POST', 'GET'])
-# def index():
-#    return render_template('index.html')
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -33,22 +30,20 @@ def upload_file():
         if file.filename == '':
             return render_template('index.html', description="Unustasid faili valida")
         if file and allowed_file(file.filename):
-            # print(file.read().decode("utf8"))
             # For saving file
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             # TODO create description
             description = "Siia tuleb muusikafaili kirjeldus" + "\nÜles laetud fail: " + file.filename
+
+            # TODO delete file
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
             return render_template('index.html', description=description)
 
     return render_template('index.html', description="Lae üles fail kui kirjeldust soovid")
 
-
-# If file is being save locally
-# @app.route('/uploads/<filename>')
-# def uploaded_file(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'],
-#                                filename)
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
